262b7ab96a256a949e0713d432ce3aab19b14cd1
1Max58 <>
2023-10-17 13:46:54 +0300

    implementation using BigDecimal type

--- src/com/example/taxcalculation/TaxCalculation.java
+++ src/com/example/taxcalculation/TaxCalculation.java
@@ -1,5 +1,8 @@
 package com.example.taxcalculation;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class TaxCalculation {
     public static void main(String[] args) {
         double netPrice = 9.99;
@@ -10,11 +13,28 @@ public class TaxCalculation {
         double grossIncome = qty * grossValue;
         double netIncome = grossIncome / netToGross;
 
+        System.out.println("---------------------------------------------------");
         System.out.println("Using double type we implemented the following:");
         System.out.println("Product costs " + netPrice + " net");
         System.out.println("Its gross value is " + grossValue);
         System.out.println("(we assume VAT of " + VAT + "%");
         System.out.println("After we sold " + qty + " pcs of this product we got " + grossIncome + " of gross income");
         System.out.println("This value excluding VAT is " + netIncome);
+
+        BigDecimal netPriceBD = BigDecimal.valueOf(9.99);
+        BigDecimal VAT_BD = BigDecimal.valueOf(23);
+        BigDecimal netToGrossBD = BigDecimal.valueOf(1.0).add(VAT_BD.multiply(BigDecimal.valueOf(0.01)));
+        BigDecimal grossValueBD = netPriceBD.multiply(netToGrossBD);
+        BigDecimal qtyBD = BigDecimal.valueOf(10_000);
+        BigDecimal grossIncomeBD = qtyBD.multiply(grossValueBD);
+        BigDecimal netIncomeBD = grossIncomeBD.divide(netToGrossBD, RoundingMode.DOWN);
+
+        System.out.println("---------------------------------------------------");
+        System.out.println("Using BigDecimal type we implemented the following:");
+        System.out.println("Product costs " + netPriceBD + " net");
+        System.out.println("Its gross value is " + grossValueBD);
+        System.out.println("(we assume VAT of " + VAT_BD + "%");
+        System.out.println("After we sold " + qtyBD + " pcs of this product we got " + grossIncomeBD + " of gross income");
+        System.out.println("This value excluding VAT is " + netIncomeBD);
     }
 }
